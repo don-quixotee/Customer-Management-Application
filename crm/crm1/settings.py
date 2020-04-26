ad524ed14c02d533c5fb94982d1ac295f3289e29
Abhishek singh <60599305+don-quixotee@users.noreply.github.com>
2020-04-27 01:41:57 +0530

    Update settings.py

--- crm/crm1/settings.py
+++ crm/crm1/settings.py
@@ -140,4 +140,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'imavicq@gmail.com'
-EMAIL_HOST_PASSWORD = 'Pussy1@bana'
+EMAIL_HOST_PASSWORD = '***********'
